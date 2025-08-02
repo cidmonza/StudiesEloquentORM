b015de697252f8be731e6f6ac06b1ab4d99547fc
Cid Monza <stinermonza@gmail.com>
2025-08-02 00:44:04 -0300

    atualização à introdução

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/teste', function ()
-{
-    $products = Product::all();
-    echo '<pre>';
-    print_r($products->toArray());
-});
+Route::get('/', [MainController::class, 'index']);
