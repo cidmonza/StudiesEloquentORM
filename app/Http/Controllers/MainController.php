8b1bae20ef32b7611e739e9d6f4dd3cef0002d13
Cid Monza <stinermonza@gmail.com>
2025-08-03 03:06:09 -0300

    explorando formas de caputurar dados do db com EORM

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use App\Models\TesteModel;
 use Illuminate\Http\Request;
 
@@ -9,8 +10,48 @@ class MainController extends Controller
 {
     public function index()
     {
-        $products = TesteModel::all()->toArray();
-        echo "<pre>";
-        print_r($products);
+        // Buscar todos os dados dos produtos
+        $results = Product::all(); // SELECT * FROM products
+
+        // foreach($results as $product){
+        //     echo "<br>";
+        //     echo $product->product_name;
+        // }
+
+        // echo $results[0]->product_name;
+
+        // Buscar todos os dados como um array associativo
+        //$results = Product::all()->toArray();
+
+        // retornar os resultados como um array de objetos stdClass
+        //$results = $this->arrayOfObject(Product::all()->toArray());
+
+        // Buscar produtos ordenados pelo nome alfabeticamente
+
+        // $results = Product::orderBy('product_name')
+        //                     ->get()
+        //                     ->toArray();
+
+        // Buscar os 3 primeiros produtos
+        //$results = Product::limit(3)->get()->toArray();
+
+        // Buscar produto pelo id
+        $results = Product::find(10)->toArray();
+
+        $this->showData($results);
+
+    }
+
+    private function showData($data){
+        echo '<pre>',
+        print_r($data);
+    }
+
+    private function arrayOfObject($data){
+        $tmp = [];
+        foreach($data as $key => $value){
+            $tmp[] = (object) $value;
+        }
+        return $tmp;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'index']);
+
